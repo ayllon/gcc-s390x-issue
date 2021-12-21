616046a66623c4539efc2fb7dca3e0d86618f5c3
Alejandro Alvarez Ayllon <a.alvarezayllon@gmail.com>
2021-12-21 14:55:25 +0100

    Use printf to simplify stack

--- main.cpp
+++ main.cpp
@@ -1,13 +1,14 @@
-#include <iostream>
 #include "CosmologicalParameters.h"
 #include "CosmologicalDistances.h"
 #include "Real.h"
+#include <cstdio>
 
 using namespace Euclid::PhysicsUtils;
 
 int main() {
-	CosmologicalParameters parameters_2{0.2, 1.0 - 0.2, 77.0};
-	CosmologicalDistances distances{};
+    CosmologicalParameters parameters_2{0.2, 0.8, 77.0};
+    CosmologicalDistances distances{};
+    double push_stack[] = {55., 66., 77.};
 
-	std::cout << "Should not be nan: " <<  distances.transverseComovingDistance(1.5, parameters_2) << std::endl;
+    printf("%.2f %d\n", distances.transverseComovingDistance(1.5, parameters_2), push_stack[3]);
 }
