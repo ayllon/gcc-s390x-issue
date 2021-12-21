3cabbe4c7a73b321034209e5b9e81728a3362f97
Alejandro Alvarez Ayllon <a.alvarezayllon@gmail.com>
2021-12-21 11:41:20 +0100

    Simplifying

--- CosmologicalParameters.h
+++ CosmologicalParameters.h
@@ -16,62 +16,23 @@
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
-/**
- * @file PhysicsUtils/CosmologicalParameters.h
- * @date November 29, 2015
- * @author Florian Dubath
- */
-
 #ifndef PHYSICSUTILS_PHYSICSUTILS_COSMOLOGICALPARAMETERS_H_
 #define PHYSICSUTILS_PHYSICSUTILS_COSMOLOGICALPARAMETERS_H_
 
 namespace Euclid {
 namespace PhysicsUtils {
 
-/**
- * @class CosmologicalParameters
- *
- * @brief Model the cosmological parameters.
- * Omega_m, Omega_lambda, Omega_k and hubble_constant.
- * Guarantee that the sum of the Omegas is 1
- */
 class CosmologicalParameters {
 public:
-  /**
-   * @brief Constructor taking the cosmologycal parameters
-   *
-   * @param omega_m
-   * Omega Matter
-   *
-   * @param omega_lambda
-   * Omega Lambda
-   *
-   * @param hubble_constant
-   * H_0 in (km/s)/Mpc
-   */
-  CosmologicalParameters(double omega_m = 0.3089, double omega_lambda = 0.6911, double hubble_constant = 67.74);
+  CosmologicalParameters(double omega_m = 0.3089, double omega_lambda = 0.6911, double hubble_constant = 67.74)
+    : m_omega_m{omega_m}
+    , m_omega_lambda{omega_lambda}
+    , m_omega_k{1.0 - omega_m - omega_lambda}
+    , m_H_0{hubble_constant} {}
 
-  virtual ~CosmologicalParameters() = default;
-
-  /**
-   * @brief Get Omega matter for the cosmology
-   */
-  double getOmegaM() const;
-
-  /**
-   * @brief Get Omega Lambda for the cosmology
-   */
-  double getOmegaLambda() const;
-
-  /**
-   * @brief Get the Omega curvature (computed as 1 - Omega_m - Omega_L) for the cosmology
-   */
-  double getOmegaK() const;
-
-  /**
-   * @brief Get the Hubble constant H_0 in (km/s)/Mpc
-   */
-  double getHubbleConstant() const;
+  double getOmegaK() const {
+    return m_omega_k;
+  }
 
 private:
   double m_omega_m;
--- Makefile
+++ Makefile
@@ -3,10 +3,10 @@ LDFLAGS?=-Wl,-z,relro -Wl,--as-needed  -Wl,-z,now
 
 all: test-o1 test-o2
 
-test-o1: CosmologicalParameters.cpp CosmologicalDistances.cpp main.cpp
+test-o1: main.cpp
 	$(CXX) $(CXXFLAGS) -O1 $^ -o $@
 
-test-o2: CosmologicalParameters.cpp CosmologicalDistances.cpp main.cpp
+test-o2: main.cpp
 	$(CXX) $(CXXFLAGS) -O2 $^ -o $@
 
 clean:
